2e9ef93698e1194d13553564cd45019621a8e053
Konstantinos Douloudis <kdoul@unipi.gr>
2021-02-02 19:02:20 +0200

    Update App.java

--- src/main/java/eu/unipi/App.java
+++ src/main/java/eu/unipi/App.java
@@ -19,8 +19,9 @@ public class App {
   public static void main(String[] args) {
     Javalin app = Javalin.create().start(8080);
 
-    app.get("/api/discover/movie", MovieController::handlePopularMovies);
-    app.get("/api/search/movie", MovieController::handleSearchMovies);
+// ALTERNATE WAY OF DECLARING ENDPOINT FUNCTIONALITY
+//     app.get("/api/discover/movie", MovieController::handlePopularMovies);
+//     app.get("/api/search/movie", MovieController::handleSearchMovies);
 
     app.get(
         "/api/discover/movie",
